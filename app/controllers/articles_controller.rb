d6ce2ccc279c61af1c95f388f0bbc7677bbc0cf3
Mykl Clason <myklclason@gmail.com>
2016-02-16 20:22:02 +0000

    Link articles to users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  #before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
   before_filter :set_article, only: [:show,:edit,:update, :destroy]
 
   def index
@@ -11,7 +11,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(article_params)
+    @article = current_user.articles.build(article_params)
     if @article.save
       flash[:success] = "Article has been created"
       redirect_to articles_path
